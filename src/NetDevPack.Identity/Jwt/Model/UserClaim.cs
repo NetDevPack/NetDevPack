8b3e86d3861e9cbcb5fe52ad598af5cd1da5315f
Eduardo Pires <falecom@eduardopires.net.br>
2020-06-05 19:42:34 -0300

    v1.0.4

--- src/NetDevPack.Identity/Jwt/Model/UserClaim.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NetDevPack.Identity.Jwt.Model
-{
-    public class UserClaim
-    {
-        public string Value { get; set; }
-        public string Type { get; set; }
-    }
-}
--- src/NetDevPack.Identity/Jwt/Model/UserResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetDevPack.Identity.Jwt.Model
-{
-    public class UserResponse
-    {
-        public string AccessToken { get; set; }
-        public double ExpiresIn { get; set; }
-        public UserToken UserToken { get; set; }
-    }
-}
