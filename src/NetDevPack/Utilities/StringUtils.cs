b42513edd1de57b82f46f07fbdfd0b56d5154d6f
Alex Dias de Oliveira <78912689+oliveira-alexdias@users.noreply.github.com>
2021-03-05 14:40:14 -0300

    Add Remove Special Chatacters
    Used regex to remove special characters in a string.

--- src/NetDevPack/Utilities/StringUtils.cs
+++ src/NetDevPack/Utilities/StringUtils.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 
 namespace NetDevPack.Utilities
 {
@@ -7,6 +7,11 @@ namespace NetDevPack.Utilities
         public static string OnlyNumbers(this string str, string input)
         {
             return new string(input.Where(char.IsDigit).ToArray());
+        }
+		
+		public static string RemoveSpecialCharacters(this string str)
+        {
+            return Regex.Replace(str, "[^a-zA-Z0-9-_.]+", "", RegexOptions.Compiled);
         }
     }
 }
