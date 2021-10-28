234080821c20c37f863269cfc8ee2dda20008386
Jan <72037282+Pomid0rchik@users.noreply.github.com>
2021-10-29 00:28:11 +0300

    Update UrlizeComparison.cs

--- src/NetDevPack.Perf/Tests/UrlizeComparison.cs
+++ src/NetDevPack.Perf/Tests/UrlizeComparison.cs
@@ -11,7 +11,6 @@ namespace NetDevPack.Perf.Tests
         private static readonly Regex UrlizeRegex = new Regex(@"[^A-Za-z0-9_\/~]+", RegexOptions.Multiline | RegexOptions.Compiled);
         private const string Title = " Tipo de dado gerado pela configuração HasColumnType<string> ";
 
-#if NET5_0_OR_GREATER || NETSTANDARD2_1
         [Benchmark]
         public string Urlize()
         {
@@ -27,23 +26,6 @@ namespace NetDevPack.Perf.Tests
 
             return editedTitle;
         }
-#else
-        [Benchmark]
-        public string Urlize()
-        {
-            var editedTitle = Title.Trim().ToLower().RemoveDiacritics();
-
-            editedTitle = UrlizeRegex.Replace(tituloEditado, "-");
-
-            if (editedTitle[0] == "-")
-                editedTitle = editedTitle.Substring(1);
-
-            if (editedTitle[editedTitle.Length - 1] == "-")
-                editedTitle = editedTitle.Substring(0, editedTitle.Length - 1);
-
-            return editedTitle;
-        }
-#endif
 
         [Benchmark]
         public string NetDevPackUrlize() => " Tipo de dado gerado pela configuração HasColumnType<string> ".Urlize();
